a0e554fcf370589b7c11537f69a98d7ca996d041
Samriddhi <samriddhisrivastava.sr727@gmail.com>
2025-08-13 15:24:52 +0530

    add getAllCourses endpoint with field restrictions

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -71,4 +71,15 @@ export const getSingleCourse = catchAsyncErrors(
       return next(new ErrorHandler(error.message, 500));
     }
   }
-);  
+); 
+
+export const getAllCourses = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await CourseModel.find().select('-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links');
+      res.status(200).json({ success: true, courses });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { uploadCourse, editCourse, getSingleCourse } from '../controllers/course.controller';
+import { uploadCourse, editCourse, getSingleCourse , getAllCourses } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenticated
  } from '../middleware/auth';
 
@@ -10,5 +10,7 @@ courseRouter.post('/create-course', isAuthenticated, authorizeRoles("admin"), up
 courseRouter.put('/edit-course/:id', isAuthenticated, authorizeRoles("admin"), editCourse);
 
 courseRouter.get('/get-course/:id', getSingleCourse);
+
+courseRouter.get('/get-courses', getAllCourses);
 
 export default courseRouter;
